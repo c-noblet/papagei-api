21a511696d80eb134ddf17e51f0b0beaba9856f9
c-noblet <c.noblet.dev@gmail.com>
2021-12-11 18:03:21 +0100

    remove local https api

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,5 @@
 import 'dotenv/config';
 import http from 'http';
-import https from 'https';
-import path from 'path';
-import fs from 'fs';
 import setBot from './discord';
 import Player from './Player';
 import setApi from './api';
@@ -10,17 +7,6 @@ import setApi from './api';
 /* Discord Player initialisation */
 const player = new Player();
 
-/* Get SSL */
-const privateKey = process.env.SSL_PRIVATE_KEY_PATH;
-const publicKey = process.env.SSL_PUBLIC_KEY_PATH;
-let credentials = {};
-
-if (privateKey && publicKey) {
-  credentials = {
-    key: fs.readFileSync(path.resolve() + privateKey),
-    cert: fs.readFileSync(path.resolve() + publicKey),
-  }
-}
 
 try {
 
@@ -31,10 +17,7 @@ try {
   /* Start the API */
   const app = setApi(player);
   const httpServer = http.createServer(app);
-  const httpsServer = https.createServer(credentials, app);
-
   httpServer.listen(8080);
-  httpsServer.listen(8443);
 
 } catch (err) {
   console.error(err);
